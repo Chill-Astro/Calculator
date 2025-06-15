c259c576bd9656221d9421e09e73027141c37499
Dev. Chill-Astro <MasterArc3435@outlook.com>
2025-06-15 14:46:56 +0530

    Restructure

---
+++ Calculator/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Calculator.ViewModels;
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace Calculator.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+}
